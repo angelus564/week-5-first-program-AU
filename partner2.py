69383a9aaa5a5743f309a222c824076abf8ee5ab
Angel Uriegas <angel.uriegas01@utrgv.edu>
2026-02-18 12:25:37 -0600

    class work assignment wed 18

---
+++ partner2.py
@@ -0,0 +1,39 @@
+# 1. Print a message to the screen
+message = "THis is a message"
+print(message)
+
+# 2. Change the message to your own words and run again
+message = "my own word"
+print(message)
+
+# 3. Print your favorite food
+print("pizza")
+
+# 4. Try printing a number instead of text
+print(123)
+
+# 5. Create a variable that stores your name
+name = "Nathan"
+# 6. Print the variable
+print(name)
+
+# 7. Change the name inside the variable and run again
+name = "Angel"
+print(name)
+# 8. Create a variable with a number
+num = 18
+# 9. Print the age
+print(num)
+# 10. Add 2 to the age and print the result
+print(num+2)
+# 11. Ask the user for their name
+print("whats your name?")
+
+# 12. Print a greeting using their name
+print("Hi " + name)
+# 13. Ask the user for a number
+user_num = input("Whats the number: ")
+# 14. Print the number they typed
+print(user_num)
+# 15. Print a goodbye message
+print("bye bye")
